a2a88b9f64c0c2a3a6a0d339c074a2b1c4068a8c
Annevd <144004647+Annevd@users.noreply.github.com>
2023-12-11 11:06:33 +0100

    rotate

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,16 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+//Mijn interacties
+
+// rotate 'click'
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', rotate)
+
+function rotate() {
+  frontend.classList.toggle('rotate')
+}
+
