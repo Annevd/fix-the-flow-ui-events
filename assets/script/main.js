8e1ff8df09f82623f48717b1c0a5ca21ef55bd18
Annevd <144004647+Annevd@users.noreply.github.com>
2023-12-12 11:46:59 +0100

    added comments with explanations

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,106 +11,106 @@ function jumpHandler() {
 
 // rotate 'click'
 
-let frontend = document.querySelector('a:nth-of-type(1)')
+let frontend = document.querySelector('a:nth-of-type(1)') //hiermee selecteer ik de eerste link en voeg hem toe aan een variabele
 
-frontend.addEventListener('click', rotate)
+frontend.addEventListener('click', rotate) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval één muisklik en geef ik de opkomende functie een naam
 
-function rotate() {
-  frontend.classList.toggle('rotate')
+function rotate() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  frontend.classList.toggle('rotate') //dit toggled de class .rotate in de CSS
 }
 
 // background 'click'
 
-let design = document.querySelector('a:nth-of-type(2)')
+let design = document.querySelector('a:nth-of-type(2)') //hiermee selecteer ik de tweede link en voeg hem toe aan een variabele
 
-design.addEventListener('click', background)
+design.addEventListener('click', background) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval één muisklik en geef ik de opkomende functie een naam
 
-function background() {
-  design.classList.toggle('background')
+function background() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  design.classList.toggle('background') //dit toggled de class .background in de CSS
 }
 
 // rainbow 'click'
 
-let and = document.querySelector('a:nth-of-type(3)')
+let and = document.querySelector('a:nth-of-type(3)') //hiermee selecteer ik de derde link en voeg hem toe aan een variabele
 
-and.addEventListener('click', rainbow)
+and.addEventListener('click', rainbow) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval één muisklik en geef ik de opkomende functie een naam
 
-function rainbow() {
-  and.classList.toggle('rainbow')
+function rainbow() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  and.classList.toggle('rainbow') //dit toggled de class .rainbow in de CSS
 }
 
 // shake 'dblclick'
 
-let development = document.querySelector('a:nth-of-type(4)')
+let development = document.querySelector('a:nth-of-type(4)') //hiermee selecteer ik de vierde link en voeg hem toe aan een variabele
 
-development.addEventListener('dblclick', shake)
+development.addEventListener('dblclick', shake) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval een dubbele muisklik en geef ik de opkomende functie een naam
 
-function shake() {
-  development.classList.toggle('shake')
+function shake() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  development.classList.toggle('shake') //dit toggled de class .shake in de CSS
 }
 
 // flip 'keydown'
 
-let sprint = document.querySelector('a:nth-of-type(5)')
+let sprint = document.querySelector('a:nth-of-type(5)') //hiermee selecteer ik de vijfde link en voeg hem toe aan een variabele
 
-document.addEventListener('keydown', flip) // document omdat het over een toetsenbord gaat
+document.addEventListener('keydown', flip) //document.addEventListener omdat je het toetsenbord wil kunnen klikken in het gehele document, niet alleen in een knop
 
-function flip(e) { // e staat voor event
-  if(e.key == "a") { // als de key gelijk is aan 'a', doe dan dit:
+function flip(e) { // e staat voor event, ook hier roep je de functie aan en zeg je eronder wat de functie moet doen
+  if(e.key == "a") { // als de toets gelijk is aan 'a', doe dan dit:
     console.log("we hebben een "+e.key) // om te testen
-    sprint.classList.toggle('flip') // toggle de .flip class
+    sprint.classList.toggle('flip') // dit toggled de .flip class in de CSS
   }
 }
 
 // blur 'mouseover'
 
-let fix = document.querySelector('a:nth-of-type(6)')
+let fix = document.querySelector('a:nth-of-type(6)') //hiermee selecteer ik de zesde link en voeg hem toe aan een variabele
 
-fix.addEventListener('mouseover', blur)
+fix.addEventListener('mouseover', blur) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval een hover met de muis en geef ik de opkomende functie een naam
 
-function blur() {
-  fix.classList.toggle('blur')
+function blur() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  fix.classList.toggle('blur') //dit toggled de class .blur in de CSS
 }
 
 // scale 'mousover'
 
-let the = document.querySelector('a:nth-of-type(7)')
+let the = document.querySelector('a:nth-of-type(7)') //hiermee selecteer ik de zevende link en voeg hem toe aan een variabele
 
-the.addEventListener('mouseover', scale)
+the.addEventListener('mouseover', scale) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval een hover met de muis en geef ik de opkomende functie een naam
 
-function scale() {
-  the.classList.toggle('scale')
+function scale() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  the.classList.toggle('scale') //dit toggled de class .scale in de CSS
 }
 
 // flicker 'dblclick'
 
-let flow = document.querySelector('a:nth-of-type(8)')
+let flow = document.querySelector('a:nth-of-type(8)') //hiermee selecteer ik de achtste link en voeg hem toe aan een variabele
 
-flow.addEventListener('dblclick', flicker)
+flow.addEventListener('dblclick', flicker) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval een dubbele muisklik en geef ik de opkomende functie een naam
 
-function flicker() {
-  flow.classList.toggle('flicker')
+function flicker() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  flow.classList.toggle('flicker') //dit toggled de class .flicker in de CSS
 }
 
 // negative 'keydown'
 
-let user = document.querySelector('a:nth-of-type(9)')
+let user = document.querySelector('a:nth-of-type(9)') //hiermee selecteer ik de negende link en voeg hem toe aan een variabele
 
-document.addEventListener('keydown', negative) //document omdat je toetsen klikt in het gehele document, niet 1 knop
+document.addEventListener('keydown', negative) //document.addEventListener omdat je het toetsenbord wil kunnen klikken in het gehele document, niet alleen in een knop
 
-function negative(e) { // e staat voor event
+function negative(e) { // e staat voor event, ook hier roep je de functie aan en zeg je eronder wat de functie moet doen
   if(e.key == "n") { // als de key gelijk is aan 'n', doe dan dit:
     console.log("we hebben een "+e.key) // om te testen
-    user.classList.toggle('negative') // toggle de .negative class
+    user.classList.toggle('negative') // dit toggled de .negative class
   }
 }
 
 // turn 'mouseover'
 
-let interface = document.querySelector('a:nth-of-type(10)')
+let interface = document.querySelector('a:nth-of-type(10)') //hiermee selecteer ik de tiende link en voeg hem toe aan een variabele
 
-interface.addEventListener('mouseover', turn)
+interface.addEventListener('mouseover', turn) //d.m.v addEventListener wordt er geluisterd naar een event, in dit geval een hover met de muis en geef ik de opkomende functie een naam
 
-function turn() {
-  interface.classList.toggle('turn')
+function turn() { //vervolgens zeg ik hier wat de functie die ik in de vorige regel heb benoemd moet doen
+  interface.classList.toggle('turn') //dit toggled de class .turn in de CSS
 }
